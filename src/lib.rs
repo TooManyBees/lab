0aaed076f60367b810e8391105f5034def539f11
Jesse Bees <jesse@toomanybees.com>
2017-01-29 22:07:21 -0500

    Make it faster with arrays instead of collecting Vecs

--- src/lib.rs
+++ src/lib.rs
@@ -5,93 +5,110 @@ pub struct Lab {
     pub b: f32,
 }
 
+fn rgb_to_xyz(rgb: [f32; 3]) -> [f32; 3] {
+    let r = rgb_to_xyz_map(rgb[0] / 255.0);
+    let g = rgb_to_xyz_map(rgb[1] / 255.0);
+    let b = rgb_to_xyz_map(rgb[2] / 255.0);
+
+    [
+        r*0.4124 + g*0.3576 + b*0.1805,
+        r*0.2126 + g*0.7152 + b*0.0722,
+        r*0.0193 + g*0.1192 + b*0.9505,
+    ]
+}
+
+fn rgb_to_xyz_map(c: f32) -> f32 {
+    (if c > 0.04045 {
+        ((c + 0.055) / 1.055).powf(2.4)
+    } else {
+        c / 12.92
+    }) * 100.0
+}
+
+fn xyz_to_lab(xyz: [f32; 3]) -> [f32; 3] {
+    let x = xyz_to_lab_map(xyz[0] / 95.047);
+    let y = xyz_to_lab_map(xyz[1] / 100.0);
+    let z = xyz_to_lab_map(xyz[2] / 108.883);
+
+    [
+        (116.0 * y) - 16.0,
+        500.0 * (x - y),
+        200.0 * (y - z),
+    ]
+}
+
+fn xyz_to_lab_map(c: f32) -> f32 {
+    if c > 0.008856 {
+        c.powf(1.0/3.0)
+    } else {
+        (c * 7.787) + ( 16.0 / 116.0 )
+    }
+}
+
+fn lab_to_xyz(lab: [f32; 3]) -> [f32; 3] {
+    let mut y = (lab[0] + 16.0) / 116.0;
+    let mut x = (lab[1] / 500.0) + y;
+    let mut z = y - (lab[2] / 200.0);
+
+    x = lab_to_xyz_map(x);
+    y = lab_to_xyz_map(y);
+    z = lab_to_xyz_map(z);
+
+    [
+        x * 95.047,
+        y * 100.0,
+        z * 108.883,
+    ]
+}
+
+fn lab_to_xyz_map(c: f32) -> f32 {
+    let raised = c.powf(3.0);
+    if raised > 0.008856 {
+        raised
+    } else {
+        (c - 16.0) / 116.0 / 7.787
+    }
+}
+
+fn xyz_to_rgb(xyz: [f32; 3]) -> [f32; 3] {
+    let x = xyz[0] / 100.0;
+    let y = xyz[1] / 100.0;
+    let z = xyz[2] / 100.0;
+
+    let r = xyz_to_rgb_map(x *  3.2406 + y * -1.5372 + z * -0.4986);
+    let g = xyz_to_rgb_map(x * -0.9689 + y *  1.8758 + z *  0.0415);
+    let b = xyz_to_rgb_map(x *  0.0557 + y * -0.2040 + z *  1.0570);
+
+    [r, g, b]
+}
+
+fn xyz_to_rgb_map(c: f32) -> f32 {
+    (if c > 0.0031308 {
+         1.055 * c.powf(1.0/2.4) - 0.055
+    } else {
+         12.92 * c
+    }) * 255.0
+}
+
 impl Lab {
     pub fn from_rgb(r: u8, g: u8, b: u8) -> Self {
-        let (x, y, z) = {
-            let r = (r as f32) / 255.0;
-            let g = (g as f32) / 255.0;
-            let b = (b as f32) / 255.0;
-            let _rgb: Vec<f32> = [r, g, b].into_iter().map(|&c| {
-                (if c > 0.04045 {
-                    ((c + 0.055) / 1.055).powf(2.4)
-                } else {
-                    c / 12.92
-                }) * 100.0
-            }).collect();
-
-            (
-                _rgb[0]*0.4124 + _rgb[1]*0.3576 + _rgb[2]*0.1805,
-                _rgb[0]*0.2126 + _rgb[1]*0.7152 + _rgb[2]*0.0722,
-                _rgb[0]*0.0193 + _rgb[1]*0.1192 + _rgb[2]*0.9505,
-            )
-        };
-
-        let (l, a, b) = {
-            let _x = x / 95.047;
-            let _y = y / 100.0;
-            let _z = z / 108.883;
-
-            let _xyz: Vec<f32> = [_x, _y, _z].into_iter().map(|&c| {
-                if c > 0.008856 {
-                    c.powf(1.0/3.0)
-                } else {
-                    (c * 7.787) + ( 16.0 / 116.0 )
-                }
-            }).collect();
-
-            (
-                (116.0 * _xyz[1]) - 16.0,
-                500.0 * (_xyz[0] - _xyz[1]),
-                200.0 * (_xyz[1] - _xyz[2]),
-            )
-        };
-
-        Lab { l: l, a: a, b: b }
+        let xyz = rgb_to_xyz([r as f32, g as f32, b as f32]);
+        let lab = xyz_to_lab(xyz);
+        Lab {
+            l: lab[0],
+            a: lab[1],
+            b: lab[2],
+        }
     }
 
-    pub fn to_rgb(&self) -> (u8, u8, u8) {
-        let (x, y, z) = {
-            let y = (self.l + 16.0) / 116.0;
-            let x = (self.a / 500.0) + y;
-            let z = y - (self.b / 200.0);
-
-            let xyz: Vec<f32> = [x, y, z].into_iter().map(|&c| {
-                let raised = c.powf(3.0);
-                if raised > 0.008856 {
-                    raised
-                } else {
-                    (c - 16.0) / 116.0 / 7.787
-                }
-            }).collect();
-
-            (
-                xyz[0] * 95.047,
-                xyz[1] * 100.0,
-                xyz[2] * 108.883,
-            )
-        };
-
-        let (r, g, b) = {
-            let x = x / 100.0;
-            let y = y / 100.0;
-            let z = z / 100.0;
-
-            let r = x *  3.2406 + y * -1.5372 + z * -0.4986;
-            let g = x * -0.9689 + y *  1.8758 + z *  0.0415;
-            let b = x *  0.0557 + y * -0.2040 + z *  1.0570;
-
-            let _rgb: Vec<f32> = [r, g, b].into_iter().map(|&c| {
-                (if c > 0.0031308 {
-                     1.055 * c.powf(1.0/2.4) - 0.055
-                } else {
-                     12.92 * c
-                } * 255.0).round()
-            }).collect();
-
-            (_rgb[0] as u8, _rgb[1] as u8, _rgb[2] as u8)
-        };
-
-        (r, g, b)
+    pub fn to_rgb(&self) -> [u8; 3] {
+        let xyz = lab_to_xyz([self.l, self.a, self.b]);
+        let rgb = xyz_to_rgb(xyz);
+        [
+            rgb[0].round() as u8,
+            rgb[1].round() as u8,
+            rgb[2].round() as u8,
+        ]
     }
 
     pub fn squared_distance(&self, other: &Lab) -> f32 {
@@ -119,7 +136,7 @@ mod tests {
         let lab = Lab { l: 66.6348, a: 52.260696, b: 14.850557 };
         assert_eq!(
             lab.to_rgb(),
-            (253, 120, 138)
+            [253, 120, 138]
         );
     }
 }
