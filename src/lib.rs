1e316c003f6dec30cce98b02a5b5440a777ca685
Jesse Bees <jesse@toomanybees.com>
2018-09-19 12:11:58 -0400

    Bump version to release bug fix

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //!
 //! Tools for converting RGB colors to L\*a\*b\* measurements.
 
-#![doc(html_root_url = "https://docs.rs/lab/0.4.3")]
+#![doc(html_root_url = "https://docs.rs/lab/0.4.4")]
 
 #[derive(Debug, PartialEq, Copy, Clone, Default)]
 pub struct Lab {
